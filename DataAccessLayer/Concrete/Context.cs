42131bab9b62c58bebec45a802f9d9134375e2fc
begumeygun <begumeygun@gmail.com>
2024-07-19 17:07:16 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,42 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+	public class Context : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=ZED_AIR_CX3\\SQLSERVEREXPRESS;User=ZED_AIR_CX3\\user;" +
+				"Password=eygun123;database=TraversalDB;Trusted_Connection=True;TrustServerCertificate=True");
+		}
+
+
+		public DbSet<About> Abouts { get; set; }
+		public DbSet<About2> About2s { get; set; }
+		public DbSet<Contact> Contacts { get; set; }
+		public DbSet<Destination> Destinations { get; set; }
+		public DbSet<Feature> Features { get; set; }
+		public DbSet<Feature2> Feature2s { get; set; }
+		public DbSet<Guide> Guides { get; set; }
+		public DbSet<Newsletter> Newsletters { get; set; }
+		public DbSet<SubAbout> SubAbouts { get; set; }
+		public DbSet<Testimonial> Testimonials { get; set; }
+
+
+
+
+
+
+
+
+
+
+	}
+}
---
+++ TatilRezervasyon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TatilRezervasyon.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
